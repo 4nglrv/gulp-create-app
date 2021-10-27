944dfe4501bdf56677cd154fcb69e8a60f552e94
hakuu1251 <hakuu1251@gmail.com>
2021-10-27 09:02:06 +0300

    Added css

--- rollup.config.js
+++ rollup.config.js
@@ -8,27 +8,31 @@ var { nodeModules } = require("./modules")
 nodeModules.include = "**/*.js"
 const project_folder = "src"
 const rollupConfig = {
-	plugins: [ 
-		babel({ 
-			exclude: 'node_modules/**',
-			presets: [["@babel/preset-env", { "modules": false }]]
-		}), 
+	plugins: [
+		babel({
+			exclude: "node_modules/**",
+			presets: [["@babel/preset-env", { modules: false }]],
+		}),
 		commonjs({
-		// non-CommonJS modules will be ignored, but you can also
-		// specifically include/exclude files
-		include: [ "./" + project_folder + "/js/index.js", "node_modules/**" ],
-	
-		// if true then uses of `global` won't be dealt with by this plugin
-		ignoreGlobal: false, // Default: false
-	
-		// if false then skip sourceMap generation for CommonJS modules
-		sourceMap: false // Default: true
-	}), nodeResolve({
-		jsnext: true,
-		main: false
-	}), inject(nodeModules), uglify() ],
+			// non-CommonJS modules will be ignored, but you can also
+			// specifically include/exclude files
+			include: ["./" + project_folder + "/js/index.js", "node_modules/**"],
+
+			// if true then uses of `global` won't be dealt with by this plugin
+			ignoreGlobal: false, // Default: false
+
+			// if false then skip sourceMap generation for CommonJS modules
+			sourceMap: false, // Default: true
+		}),
+		nodeResolve({
+			jsnext: true,
+			main: false,
+		}),
+		inject(nodeModules),
+	],
 }
 
-// TODO: Make dev hosting
+// TODO: Make dev hosting faster
+// ** https://stackoverflow.com/questions/45785357/how-to-disable-tree-shaking-in-rollupjs **
 
 module.exports = rollupConfig
